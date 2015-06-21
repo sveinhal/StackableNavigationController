9bc1736613b66391a743b83fd007d6b95b0e6a25
Svein Halvor Halvorsen <sveinhal@appear.in>
2015-06-21 16:46:22 +0200

    Initial Commit

---
+++ StackableNavigationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackableNavigationController
+//
+//  Created by Svein Halvor Halvorsen on 21.06.15.
+//  Copyright (c) 2015 Svein Halvor Halvorsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
